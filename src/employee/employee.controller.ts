dc1f6426dcb91f66c02419ac4ab4482471b94cb7
ParvendraRajput18 <Parvendra.Rajput@adixoo.com>
2024-11-29 19:14:38 +0530

    Rest API

---
+++ src/employee/employee.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+import { Prisma } from '@prisma/client';
+import { query } from 'express';
+
+@Controller('employee')
+export class EmployeeController {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  @Post()
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.employeeService.create(createEmployeeDto);
+  }
+
+  @Get()
+  findAll(@Query('role')role?:'INTERN'|'ENGINEER'|'AMDIN') {
+
+    return this.employeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.employeeService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput) {
+    return this.employeeService.update(+id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeeService.remove(+id);
+  }
+}
